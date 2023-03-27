39c8ffc267f1353aa261d8d61f40e2153576bc8c
Maxime Lalisse <maxime.lalisse@gmail.com>
2023-03-27 14:38:36 +0545

    feat: typography + daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [{
+      mytheme: {
+        "primary": "#3b82f6",
+        "secondary": "#F6AF3B",
+        "accent": "#36D399",
+        "neutral": "#3D4451",
+        "base-100": "#FFFFFF",
+        "info": "#3b82f6",
+        "success": "#36D399",
+        "warning": "#F6AF3B",
+        "error": "#F87272",
+      }
+    }]
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
